b2d171b353aa0e21bb3f1c4f191d96c4fdf37a80
CHL <1354100690@qq.com>
2018-07-09 22:41:29 +0800

    add 106 Leetcode

---
+++ LeetCode/106. Construct Binary Tree from Inorder and Postorder Traversal/ReconstructBinaryTree.py
@@ -0,0 +1,65 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            inorder = stringToIntegerList(line)
+            line = lines.next()
+            postorder = stringToIntegerList(line)
+
+            ret = Solution().buildTree(inorder, postorder)
+
+            out = treeNodeToString(ret)
+            print
+            out
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
